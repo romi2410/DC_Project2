ad7ac14ef16ef2e71990d6d6e92579001284d3e1
Romi Padam <romi@cometnet-10-21-51-62.utdallas.edu>
2018-04-03 12:42:50 -0500

    indentation corrected

--- src/dc_project2/DC_Project2.java
+++ src/dc_project2/DC_Project2.java
@@ -1,4 +1,5 @@
 package dc_project2;
+
 import java.util.Scanner;
 import java.io.*;
 import java.util.HashMap;
@@ -14,82 +15,109 @@ public class DC_Project2 {
   static Logger logger = LoggerFactory.getLogger(DC_Project2.class);
 
   public static void main(String[] args) throws IOException {
-        logger.info("Hello World");
-        if(args.length>=2 && args[1].equalsIgnoreCase("test")){
-          test = true;
-          System.out.println("Running in test (host will be localhost)");
+        //logger.info("Hello World");
+        
+        if(args.length>=2 && args[1].equalsIgnoreCase("test"))
+        {
+            test = true;
+            System.out.println("Running in test (host will be localhost)");
         }
-        try{
-            Scanner sc=new Scanner(new File(args[0]));
+        
+        try
+        {
+            Scanner sc = new Scanner(new File(args[0]));
             HashMap<Integer, Node> nodes = parseLines(sc);
             
-            for(Node node: nodes.values()){
-              uids2ports.put(node.uid, node.port);
-              uids2hosts.put(node.uid, node.hostname);
+            for(Node node: nodes.values())
+            {
+                uids2ports.put(node.uid, node.port);
+                uids2hosts.put(node.uid, node.hostname);
             }
+            
             System.out.println("\n Mapping from UIDs to Ports:");
             nodes.values().forEach(node -> System.out.println(node.uid + " -> " + uids2hosts.get(node.uid) + ":" + uids2ports.get(node.uid)));
             System.out.println();
         }
-        catch(IOException e){
+        catch(IOException e)
+        {
             System.out.println("File " + args[0] + " not found");
             System.exit(0);
         }
-  }
+    }
   
     public static HashMap<Integer, Node> parseLines(Scanner sc){
-        HashMap<Integer, Node> nodes = new HashMap<Integer, Node>();
+        
+        HashMap<Integer, Node> nodes = new HashMap<>();
+        HashSet<Integer> started = new HashSet<>();
         int numNodes = 0;
-        while(numNodes==0){
-          String line = sc.nextLine();
-          if(!(line.startsWith("#") || line.trim().length()==0)){
-            numNodes = Integer.parseInt(line.trim());
-          }
-        }
-        // Create nodes
-        for(int i=0; i<numNodes; i++){
-          String line = sc.nextLine();
-          if(!(line.startsWith("#") || line.trim().length()==0)){
-            String[] params = line.trim().split("\\s+");
-            Node node = parseLine_Node(params);
-            nodes.put(node.uid, node);
-          }
-          else i--;
-        }
-        // Check all servers started
-        HashSet<Integer> started = new HashSet<Integer>();
-        while(started.size()<numNodes){
-          for(Node n: nodes.values())
-            if(n.server)
-              started.add(n.uid);
-        }
-        // Create edges
         int numEdges = 0;
-        while(sc.hasNext()){
-          String line = sc.nextLine();
-          if(!(line.startsWith("#") || line.trim().length()==0)){
-            String[] params = line.trim().split("\\s+");
-            double w = Double.parseDouble(params[1]);
-            String[] tuple = params[0].substring(1, params[0].length()-1).split(",");
-            Node node1 = nodes.get(Integer.parseInt(tuple[0]));
-            Node node2 = nodes.get(Integer.parseInt(tuple[1]));
-            node1.connectTo(node2.hostname, node2.port, node2.uid, w);
-            node2.connectTo(node1.hostname, node1.port, node1.uid, w);
-            numEdges += 2;
+        
+        // Reaading number of nodes from config file
+        while(numNodes==0)
+        {
+            String line = sc.nextLine();
+            if(!(line.startsWith("#") || line.trim().length() == 0))
+            {
+                numNodes = Integer.parseInt(line.trim());
+            }
+        }
+        
+        // Create nodes
+        for(int i=0; i < numNodes; i++)
+        {
+            String line = sc.nextLine();
+            if(!(line.startsWith("#") || line.trim().length() == 0))
+            {
+                String[] params = line.trim().split("\\s+");
+                Node node = parseLine_Node(params);
+                nodes.put(node.uid, node);
+            }
+            else 
+                i--;
+        }
+        
+        // Check all servers started
+        while(started.size()<numNodes)
+        {
+            for(Node n: nodes.values())
+                if(n.server)
+                    started.add(n.uid);
+        }
+        
+        // Create edges
+        while(sc.hasNext())
+        {
+            String line = sc.nextLine();
+            
+            if(!(line.startsWith("#") || line.trim().length()==0))
+            {
+                String[] params = line.trim().split("\\s+");
+                double w = Double.parseDouble(params[1]);
+                String[] tuple = params[0].substring(1, params[0].length()-1).split(",");
+                Node node1 = nodes.get(Integer.parseInt(tuple[0]));
+                Node node2 = nodes.get(Integer.parseInt(tuple[1]));
+                node1.connectTo(node2.hostname, node2.port, node2.uid, w);
+                node2.connectTo(node1.hostname, node1.port, node1.uid, w);
+                numEdges += 2;
           }
         }
+        
         // Check all edges created
         int edgeCnt = 0;
-        while(edgeCnt < numEdges){
-          edgeCnt = 0;
-          for(Node node: nodes.values())
-            edgeCnt += node.numEdges;
+        while(edgeCnt < numEdges)
+        {
+            edgeCnt = 0;
+            for(Node node: nodes.values())
+                edgeCnt += node.numEdges;
         }
         // Initiate GHS
+        
         for(Node node: nodes.values())
-          node.initGHS();
+            node.initGHS();
+        
         return nodes;
     }
+    
     public static Node parseLine_Node(String[] nodeParams){
         String uid = nodeParams[0];
         String hostname = nodeParams[1];
