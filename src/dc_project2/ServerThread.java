60d389e56a18fe2e40b62f36191b8444e6288166
Karan Shukla <shuklak13@gmail.com>
2018-04-13 23:34:11 -0500

    Merge ClientManager classes into ServerThread

--- src/dc_project2/ServerThread.java
+++ src/dc_project2/ServerThread.java
@@ -1,6 +1,10 @@
 package dc_project2;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -41,4 +45,78 @@ public class ServerThread extends Thread{
         ex.printStackTrace();
     }
   }
+}
+
+
+
+class ClientManager implements Runnable {
+	
+    private Socket client;
+    Node owner;
+
+    public ClientManager(Socket client, Node owner) {
+      this.client = client;
+      this.owner = owner;
+    }
+
+    @Override
+    public void run() {
+      try {
+        String line;
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(client.getInputStream()));
+
+        while ((line = in.readLine()) != null){ handleMsg(line); }
+	} catch(IOException e) {
+            e.printStackTrace();
+      }
+    }
+    
+    public void handleMsg(String m){
+      Object message;
+      try {
+         ByteArrayInputStream bi = new ByteArrayInputStream(m.getBytes());
+         ObjectInputStream si = new ObjectInputStream(bi);
+         message = si.readObject();
+         owner.ghs.handleMsg(message);
+       } catch (Exception e) {
+           System.out.println(e);
+       }
+    }
+}
+
+class ClientManagerSynchronizer implements Runnable {
+    private Socket client;
+    Synchronizer sync;
+
+    public ClientManagerSynchronizer(Socket client, Synchronizer sync) {
+      this.client = client;
+      this.sync = sync;
+    }
+
+    @Override
+    public void run() {
+      try {
+            String line;
+            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            
+            while ((line = in.readLine()) != null){
+                handleMsg(line);
+            }
+	} catch(IOException e) {
+            e.printStackTrace();
+      }
+    }
+    
+    public void handleMsg(String m){
+      MWOEMsg message;
+      try {
+         ByteArrayInputStream bi = new ByteArrayInputStream(m.getBytes());
+         ObjectInputStream si = new ObjectInputStream(bi);
+         message =(MWOEMsg) si.readObject();
+         sync.handleMsg(message);
+       } catch (Exception e) {
+           System.out.println(e);
+       }
+    }
 }
